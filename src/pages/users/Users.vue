2cb37a34155fdb19b06d958ba63710e45aa051bc
Matthew <ricklefsmatthew1003@gmail.com>
2022-08-22 16:11:26 -0500

    adds users dir to src directory, mounts axios.get to populate existing users from the db

---
+++ src/pages/users/Users.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="table-responsive">
+    <table class="table table-striped table-sm">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Name</th>
+          <th scope="col">Email</th>
+          <th scope="col">Role</th>
+          <th scope="col">Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="user in users" :key="user.id">
+          <td>{{ user.id }}</td>
+          <td>{{ user.first_name }} {{ user.last_name }}</td>
+          <td>{{ user.email }}</td>
+          <td>{{ user.role.name }}</td>
+          <!-- <td>{{ user.action }}</td> -->
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script lang="ts" >
+import { onMounted, ref } from "vue";
+import axios from "axios";
+
+export default {
+  name: "UsersComponent",
+  setup() {
+    const users = ref([]);
+    onMounted(async () => {
+      const { data } = await axios.get("users");
+
+      users.value = data.data;
+    });
+
+    return {
+      users,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
