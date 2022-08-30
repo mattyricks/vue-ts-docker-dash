adade61603aea1b5c7e5113331339437425a9425
Matthew <ricklefsmatthew1003@gmail.com>
2022-08-30 12:06:27 -0500

    implements pagination functionality for user table

--- src/pages/users/Users.vue
+++ src/pages/users/Users.vue
@@ -11,16 +11,30 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="user in users" :key="user.id">
-          <td>{{ user.id }}</td>
-          <td>{{ user.first_name }} {{ user.last_name }}</td>
-          <td>{{ user.email }}</td>
-          <td>{{ user.role.name }}</td>
+        <tr v-for="user in users" :key="user['id']">
+          <td>{{ user["id"] }}</td>
+          <td>{{ user["first_name"] }} {{ user["last_name"] }}</td>
+          <td>{{ user["email"] }}</td>
+          <td>{{ user["role.name"] }}</td>
           <!-- <td>{{ user.action }}</td> -->
         </tr>
       </tbody>
     </table>
   </div>
+
+  <nav>
+    <ul class="pagination">
+      <li class="page-item">
+        <a href="javascript:void(0)" class="page-link" @click="prev"
+          >Previous</a
+        >
+      </li>
+
+      <li class="page-item">
+        <a href="javascript:void(0)" class="page-link" @click="next">Next</a>
+      </li>
+    </ul>
+  </nav>
 </template>
 
 <script lang="ts" >
@@ -31,14 +45,36 @@ export default {
   name: "UsersComponent",
   setup() {
     const users = ref([]);
-    onMounted(async () => {
-      const { data } = await axios.get("users");
+    const page = ref(1);
+    const lastPage = ref(0);
+
+    const load = async () => {
+      const { data } = await axios.get(`users?page=${page.value}`);
 
       users.value = data.data;
-    });
+      lastPage.value = data.meta.last_page;
+    };
+
+    onMounted(load);
+
+    const next = async () => {
+      if (page.value < lastPage.value) {
+        page.value++;
+        await load();
+      }
+    };
+
+    const prev = async () => {
+      if (page.value > 1) {
+        page.value--;
+        await load();
+      }
+    };
 
     return {
       users,
+      next,
+      prev,
     };
   },
 };
